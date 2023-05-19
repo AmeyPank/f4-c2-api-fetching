0833a87c39e9524bcb3c52c2f5bffe73b1383bfa
AmeyPank <pankantiameya@gmail.com>
2023-05-20 00:57:22 +0530

    Fetching Cryptocurrency api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import TableRow from './TebleRow.js';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Top 10 Cryptocurrencies</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <TableRow key={item.id} data={item} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TebleRow.js
@@ -0,0 +1,25 @@
+
+
+// TableRow.js
+
+import React from 'react';
+import "./App.css"
+const TableRow = ({ data }) => {
+  const { name, id, image, symbol, current_price, total_volume } = data;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{id}</td>
+      <td>
+        <img id='img' src={image} alt={name} />
+      </td>
+      <td>{symbol}</td>
+      <td>{current_price}</td>
+      <td>{total_volume}</td>
+    </tr>
+  );
+};
+
+export default TableRow;
+
